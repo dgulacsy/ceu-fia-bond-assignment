816722ab85b252f40bdd82ea4e8f7edcb626819b
dgulacsy <gulacsydominik@gmail.com>
2022-01-18 20:05:15 +0000

    add back-end without validations

--- app.py
+++ app.py
@@ -1,7 +1,85 @@
-from flask import Flask, render_template
+from datetime import datetime as dt, timedelta as td
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ryyohlwbhegroa:0fc7c5e5b5c20fd87c8ac45f49862b3decce41ff9f995cd4fcb6a13845f44d76@ec2-52-214-178-113.eu-west-1.compute.amazonaws.com:5432/d308ve33cr91fu'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# app.secret_key = 'secret'
+
+db = SQLAlchemy(app)
+
+class Portfolio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cet_timestamp = db.Column(db.DateTime())
+    team_name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    asset1 = db.Column(db.String(100))
+    asset2 = db.Column(db.String(100))
+    asset3 = db.Column(db.String(100))
+    asset4 = db.Column(db.String(100))
+    asset5 = db.Column(db.String(100))
+    asset6 = db.Column(db.String(100))
+    asset7 = db.Column(db.String(100))
+    weight1 = db.Column(db.Float())
+    weight2 = db.Column(db.Float())
+    weight3 = db.Column(db.Float())
+    weight4 = db.Column(db.Float())
+    weight5 = db.Column(db.Float())
+    weight6 = db.Column(db.Float())
+    weight7 = db.Column(db.Float())
+
+    def __init__(self, cet_timestamp, team_name, email, asset1, asset2, asset3, asset4, asset5, asset6, asset7, weight1, weight2, weight3, weight4, weight5, weight6, weight7):
+        self.cet_timestamp = cet_timestamp
+        self.team_name = team_name
+        self.email = email
+        self.asset1 = asset1
+        self.asset2 = asset2
+        self.asset3 = asset3
+        self.asset4 = asset4
+        self.asset5 = asset5
+        self.asset6 = asset6
+        self.asset7 = asset7
+        self.weight1 = weight1
+        self.weight2 = weight2
+        self.weight3 = weight3
+        self.weight4 = weight4
+        self.weight5 = weight5
+        self.weight6 = weight6
+        self.weight7 = weight7
 
 @app.route("/")
-def index():
-    return render_template('test.html')
+def main():
+    return render_template('main.html')
+
+@app.route("/add_portfolio", methods=['POST'])
+def add_portfolio():
+    cet_timestamp = dt.now() + td(hours=1)
+    team_name = request.form["team_name"]
+    email = request.form["email"]
+    asset1 = request.form["asset1"]
+    asset2 = request.form["asset2"]
+    asset3 = request.form["asset3"]
+    asset4 = request.form["asset4"]
+    asset5 = request.form["asset5"]
+    asset6 = request.form["asset6"]
+    asset7 = request.form["asset7"]
+    weight1 = request.form["weight1"]
+    weight2 = request.form["weight2"]
+    weight3 = request.form["weight3"]
+    weight4 = request.form["weight4"]
+    weight5 = request.form["weight5"]
+    weight6 = request.form["weight6"]
+    weight7 = request.form["weight7"]
+    entry = Portfolio(cet_timestamp, team_name, email, asset1, asset2, asset3, asset4, asset5, asset6, asset7, weight1, weight2, weight3, weight4, weight5, weight6, weight7)
+    db.session.add(entry)
+    db.session.commit()
+    return render_template("success.html")
+
+@app.route("/success")
+def success():
+    return render_template('success.html')
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
