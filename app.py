924a012034ea2efda167c4b5e479c2878c55d5af
dgulacsy <gulacsydominik@gmail.com>
2022-01-19 21:01:26 +0000

    add data table download

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from datetime import datetime as dt, timedelta as td
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, send_from_directory, current_app
 from flask_sqlalchemy import SQLAlchemy
+import csv
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ryyohlwbhegroa:0fc7c5e5b5c20fd87c8ac45f49862b3decce41ff9f995cd4fcb6a13845f44d76@ec2-52-214-178-113.eu-west-1.compute.amazonaws.com:5432/d308ve33cr91fu'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["TEMP"] = "temp"
 # app.secret_key = 'secret'
 
 db = SQLAlchemy(app)
@@ -74,12 +77,31 @@ def add_portfolio():
     entry = Portfolio(cet_timestamp, team_name, email, asset1, asset2, asset3, asset4, asset5, asset6, asset7, weight1, weight2, weight3, weight4, weight5, weight6, weight7)
     db.session.add(entry)
     db.session.commit()
-    return render_template("success.html")
+    return redirect(url_for("success"))
 
 @app.route("/success")
 def success():
     return render_template('success.html')
 
+@app.route("/download/<path:filename>'", methods=['GET', 'POST'])
+def download(filename):
+    print("Initiating data dump...")
+    outfile = open('temp/'+filename,'w')
+    outcsv = csv.writer(outfile)
+    records = db.session.query(Portfolio).all()
+    # dump header
+    outcsv.writerow([column.name for column in Portfolio.__mapper__.columns])
+    # dump data
+    [outcsv.writerow([getattr(curr, column.name) for column in Portfolio.__mapper__.columns]) for curr in records]
+    outfile.close()
+    temp = os.path.join(current_app.root_path, app.config['TEMP'])
+    try:
+        print("sending file")
+        return send_from_directory(temp, filename=filename, as_attachment=True, cache_timeout=0)
+    except FileNotFoundError:
+        abort(404)
+
 if __name__ == '__main__':
     db.create_all()
+    db.session.commit()
     app.run(debug=True)
